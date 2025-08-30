07e13b12a88221c48ddfbc0d8f24fa4f58a525cc
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-08-30 09:33:22 +0530

    date string generation updated

--- backend/utils/getDateString.js
+++ backend/utils/getDateString.js
@@ -1,5 +1,12 @@
 const getDateString = (date) => {
-  const new_date = new Date(date);
+  return Object.entries(date).reduce((date_obj, [key, obj]) => {
+    let new_date = new Date(obj.date);
+    let iso_date = `${new_date.getFullYear()}-${String(
+      new_date.getMonth() + 1
+    ).padStart(2, "0")}-${String(new_date.getDate()).padStart(2, "0")}`;
+    date_obj[key] = { date: new_date, iso_date, time: obj.time };
+    return date_obj;
+  }, {});
 };
 
 export default getDateString;
