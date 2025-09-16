7b285667ceedc69b20889b6cd360547ee5a7df0b
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-09-16 14:46:35 +0530

    (email):email configuration corrected

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -32,7 +32,7 @@ const Form = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ data }),
+        body: JSON.stringify(data),
       });
       const result = await response.json();
       if (response.ok) {
